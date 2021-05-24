122496094cde04fbd090e479ff4b94d273d114d2
Alba <alba@zeelo.co>
2021-05-24 15:58:20 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h1 class="title container">Current Weather</h1>
+      <Weather v-bind:llista-ciutats="Europa"></Weather>
+      <Weather v-bind:llista-ciutats="Asia"></Weather>
+      <Weather v-bind:llista-ciutats="Africa"></Weather>
+      <Weather v-bind:llista-ciutats="America"></Weather>
+      <Weather v-bind:llista-ciutats="Oceania"></Weather>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Weather from "@/components/Weather";
+
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      selectedContinente: "Todos",
+      toggled: false,
+      fechaDia: null,
+      fechaMes: null,
+      fechaYear: null,
+      continentes:[
+        "Todos",
+        "Europa",
+        "Asia",
+        "America",
+        "Africa",
+        "Oceania",
+      ],
+      Europa: [
+        "Barcelona",
+        "Lleida",
+        "Zaragoza",
+        "Sevilla",
+        "Madrid",
+        "París",
+        "Viborg",
+        "Londres",
+        "Berlin",
+      ],
+      Asia:[
+        "Lahore",
+        "Beijing",
+        "Kabul",
+        "Tehran",
+        "Seoul",
+        "Tokyo",
+        "Shanghai",
+      ],
+      America:[
+        "New York",
+        "New Jersey",
+        "Toronto",
+        "Medellin",
+        "Buenos Aire",
+      ],
+      Africa:[
+        "Nairobi",
+        "Cairo",
+        "Luanda",
+        "Acra",
+        "Dakar",
+      ],
+      Oceania:[
+        "Sydney",
+        "Brisbane",
+        "Melbourne",
+        "Canberra",
+        "Auckland",
+      ],
+      monthNames: [
+        "Enero",
+        "Febrero",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre"],
+    }
+  },
+  components: {
+    Weather
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="pb-3">
+    <div class="row pl-2">
+      <div class="col-6">
+        <select class="form-control" v-model="selectedCity">
+          <option v-for="ciutat in llistaCiutats" v-bind:key="ciutat">{{ciutat}}</option>
+        </select>
+      </div>
+    </div>
+    <WeatherNow v-bind:ciudad-selecionada=selectedCity></WeatherNow>
+  </section>
+</template>
+
+<script>
+import WeatherNow from "@/components/WeatherNow";
+
+export default {
+  name: "InfoMeteo",
+  props: {
+    llistaCiutats: Array,
+    continentes: Array,
+  },
+  data(){
+    return {
+      selectedCity: this.llistaCiutats[0]
+    }
+  },
+  mounted: function (){
+    return{
+      selectedCity:this.llistaCiutats[0]
+    }
+  },
+  components: {
+    WeatherNow,
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
